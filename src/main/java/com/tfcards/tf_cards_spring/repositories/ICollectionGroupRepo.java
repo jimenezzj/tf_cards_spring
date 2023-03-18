6a41eb8bed2dc56e9cd0e32a3ab5da8a455e6c23
Jonathan MS <jimenezzj@hotmail.com>
2023-03-17 21:39:13 -0600

    add derived queries to repos

--- src/main/java/com/tfcards/tf_cards_spring/repositories/ICollectionGroupRepo.java
+++ src/main/java/com/tfcards/tf_cards_spring/repositories/ICollectionGroupRepo.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.tfcards.tf_cards_spring.domain.CollectionGroup;
 
-public interface ICollectionGroupRepo extends CrudRepository<CollectionGroup, Long> {
+import java.util.Collection;
+import java.util.Set;
 
+public interface ICollectionGroupRepo extends CrudRepository<CollectionGroup, Long> {
+    Set<CollectionGroup> findCollectionGroupByReleaseYearIn(Collection<Integer> years);
 }
--- src/main/java/com/tfcards/tf_cards_spring/repositories/IFigureRepository.java
+++ src/main/java/com/tfcards/tf_cards_spring/repositories/IFigureRepository.java
@@ -1,9 +1,17 @@
 package com.tfcards.tf_cards_spring.repositories;
 
+import com.tfcards.tf_cards_spring.domain.EFigureSize;
 import org.springframework.data.repository.CrudRepository;
 
 import com.tfcards.tf_cards_spring.domain.Figure;
 
-public interface IFigureRepository extends CrudRepository<Figure, Long>{
-    
+import java.util.Collection;
+import java.util.Set;
+
+public interface IFigureRepository extends CrudRepository<Figure, Long> {
+    Set<Figure> findByReleaseYearIn(Collection<Integer> years);
+
+    Set<Figure> findByNameContaining(String nameLike);
+
+    Set<Figure> findBySizeEquals(EFigureSize size);
 }
