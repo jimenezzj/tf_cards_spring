32ce6ec6a886783a3a2bf93a7a1a7a28a167841f
Jonathan MS <jimenezzj@hotmail.com>
2023-03-31 23:13:44 -0600

    add create user to user resource controller

--- src/main/java/com/tfcards/tf_cards_spring/controllers/UserResourceController.java
+++ src/main/java/com/tfcards/tf_cards_spring/controllers/UserResourceController.java
@@ -1,9 +1,16 @@
 package com.tfcards.tf_cards_spring.controllers;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tfcards.tf_cards_spring.domain.UserTf;
 import com.tfcards.tf_cards_spring.services.IUserService;
@@ -22,5 +29,16 @@ public class UserResourceController {
     public UserTf getUser(@PathVariable String id) {
         var foundUser = this.userService.getUser(Long.valueOf(id));
         return foundUser;
+    }
+
+    @PostMapping(path = { "/", "" })
+    public ResponseEntity<UserTf> createUser(@RequestBody UserTf newUser) {
+        var storedUser = this.userService.save(newUser);
+        URI crrLocation = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(storedUser.getId())
+                .toUri();
+        return ResponseEntity.created(crrLocation).build();
+        // return null;
     }
 }
